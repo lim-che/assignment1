fdc7f6c695e8de2abe4f04d0496ad85f33cfd224
phillip yang <phillipyang1179@gmail.com>
2021-10-28 02:26:11 -0700

    first

---
+++ src/components/timers/Countdown.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Count from "../../utils/Count";
+import Card from "../../UI/Card";
+
+
+class Countdown extends React.Component {
+
+  render() {
+    return (<Card><Count></Count></Card>);
+  }
+}
+
+export default Countdown;
